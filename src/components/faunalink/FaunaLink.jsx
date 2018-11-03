c6a71880efa770b5a6d33382ece60f72d4630fda
Ion <19718355+Nei-V@users.noreply.github.com>
2018-11-03 04:13:03 +0200

    problem with reading from db

--- src/components/faunalink/FaunaLink.jsx
+++ src/components/faunalink/FaunaLink.jsx
@@ -9,12 +9,13 @@ class FaunaLink extends React.Component {
     constructor(props) {
         super(props);
         this.addFriend = this.addFriend.bind(this);
-        this.insertManyPosts = this.insertManyPosts.bind(this);
-        this.showPostByRef = this.showPostByRef.bind(this);
-        this.showPostByTitle = this.showPostByTitle.bind(this);
-        this.showAllPosts = this.showAllPosts.bind(this);
-        this.deleteAllPosts = this.deleteAllPosts.bind(this);
-        this.deleteOnePost = this.deleteOnePost.bind(this);
+        this.getFriendByRef = this.getFriendByRef.bind(this);
+        this.getFriendByName = this.getFriendByName.bind(this);
+        this.getAllFriends = this.getAllFriends.bind(this);
+        this.deleteOneFriend = this.deleteOneFriend.bind(this);
+        this.compareTwoFriends = this.compareTwoFriends.bind(this);
+        //this.insertManyPosts = this.insertManyPosts.bind(this);
+        //this.deleteAllPosts = this.deleteAllPosts.bind(this);
 
     }
     
@@ -30,6 +31,7 @@ class FaunaLink extends React.Component {
         e.preventDefault();
         console.log('new friend');
         let name = "ion" + Math.floor(Math.random() * 1000);
+        let second_name = "secondName" + Math.floor(Math.random() * 1000);
         let age = Math.floor(Math.random() * 100);
         client.query(
             q.Create(
@@ -40,65 +42,73 @@ class FaunaLink extends React.Component {
                     "answer1": `${age}`,
                     "question2": `where ${name} lives?`,
                     "answer2": `${name}inIsrael`,
-                    "email": `${name}@gmail.com`
+                    "email": `${name}@gmail.com`,
+                    "second_name":`${second_name}`,
+                    "responseRef":``
                   } }))
             .then((ret) => console.log(ret))
     }
 
-    insertManyPosts(e) {
+
+    deleteOneFriend(e) {
         client = new faunadb.Client({ secret: FAUNA_SECRET });
         e.preventDefault();
-        console.log('submit many posts func');
-        let number = Math.floor(Math.random() * 1000);
-        client.query(
-            q.Map(
-                [`instance number ${number}  random-from multiple`,
-                `instance number ${number}  random-from multiple`,
-                `instance number ${number}  random-from multiple`],
-                q.Lambda("post_title", q.Create(
-                    q.Class("friends"), { data: { title: q.Var("post_title") } }))))
-            .then((ret) => console.log(ret))
+        console.log("delete one friend func by");
+        client.query(q.Delete(q.Ref(q.Class("friends"), "214898311208894981"))).then((ret) => console.log(ret))
+            .catch((ret) => console.log(ret))
     }
 
-    showPostByRef(e) {
+    getFriendByRef(e) {
         client = new faunadb.Client({ secret: FAUNA_SECRET});
         e.preventDefault();
-        let idRef="214844684606899717";
-        console.log("show post by ref func");
-        client.query(q.Get(q.Ref(q.Class("friends"), idRef))).then((ret) => document.getElementById("showByRef").innerHTML = ret.data.title);
-
+        let idRef="214896159631606277";
+        console.log("getFriendByRef func");
+        client.query(q.Get(q.Ref(q.Class("friends"), idRef))).then((ret) => document.getElementById("getByRef").innerHTML = ret.data.name);
     }
 
-    showPostByTitle(e) {
-        //client = new faunadb.Client({ secret: FAUNA_SECRET});
+    getFriendByName(e) {
+        client = new faunadb.Client({ secret: FAUNA_SECRET});
         e.preventDefault();
-        console.log("show post by title func");
+        console.log("getFriendByName func");
+       
         client.query(
             q.Get(
-                q.Match(q.Indexes("friends"), "first post through site")))
-            .then((ret) => console.log("resolve in showPosts by title",ret));
+                q.Match(q.Index("a1"), "ion")))
+            .then((ret) => console.log("resolve in getFriendByName",ret));
         /* client.query(
             q.Get(
-                q.Match(q.Index("all_friends"), "first post through site")))
-            .then((ret) => document.getElementById("showByTitle").innerHTML = ret.data); */
+                q.Match(q.Index("all_friends"), "ion")))
+            .then((ret) => document.getElementById("getByName").innerHTML = ret.data); */
     }
 
-    showAllPosts(e) {
+    getAllFriends(e) {
         client = new faunadb.Client({ secret: FAUNA_SECRET });
         e.preventDefault();
-        console.log("show all posts func");
+        console.log("getAllFriends func");
         client.query(
             q.Paginate(q.Match(q.Index("all_friends"))))
             .then((ret) => ret.data.forEach(function (index) {
                 var p = document.createElement("p");
                 p.innerText = JSON.stringify(index.value);
-                document.getElementById("showAllPosts").appendChild(p);
+                document.getElementById("allFriends").appendChild(p);
+                console.log(index.value);
             }));
     }
 
-    
+    compareTwoFriends(e){
+        client = new faunadb.Client({ secret: FAUNA_SECRET});
+        e.preventDefault();
+        console.log("compare func");
+        let identical=false;
+        let responseRef="214896159631606277";
+        let originalRef="214896159631606277";
+        let countCorrectAns=0;
+        console.log("number of correct answers:",countCorrectAns);
+        document.getElementById("compare").innerHTML=identical;
+    }
 
-    deleteAllPosts(e) {
+
+   /*  deleteAllPosts(e) {
         client = new faunadb.Client({ secret: FAUNA_SECRET });
         e.preventDefault();
         console.log("delete all posts func");
@@ -114,18 +124,28 @@ class FaunaLink extends React.Component {
         }) ).then(c.map((index)=>{deletePost(index)}));
         
         console.log(all);
-    }
+    } */
+
+
     //Paginate(Match(Index("customer_id_filter"))).size(Value(pageSize));
     //q.Paginate(q.Match(q.Index("all_posts_in_second_app")),size=8)
 
 
-    deleteOnePost(e) {
+/* 
+    insertManyPosts(e) {
         client = new faunadb.Client({ secret: FAUNA_SECRET });
         e.preventDefault();
-        console.log("delete one posts func by");
-        client.query(q.Delete(q.Ref(q.Class("friends"), "214841931368235525"))).then((ret) => console.log(ret))
-            .catch((ret) => console.log(ret))
-    }
+        console.log('submit many posts func');
+        let number = Math.floor(Math.random() * 1000);
+        client.query(
+            q.Map(
+                [`instance number ${number}  random-from multiple`,
+                `instance number ${number}  random-from multiple`,
+                `instance number ${number}  random-from multiple`],
+                q.Lambda("post_title", q.Create(
+                    q.Class("friends"), { data: { title: q.Var("post_title") } }))))
+            .then((ret) => console.log(ret))
+    } */
 
     
 
@@ -135,23 +155,20 @@ class FaunaLink extends React.Component {
             <br />,
             <button onClick={this.addFriend}>create one friend</button>,
             <br />,
-            <button onClick={this.insertManyPosts}>create many posts</button>,
-            <br />,
             <h1>show posts</h1>,
-            <button onClick={this.showPostByRef}>Show post by ref</button>,
-            <p id="showByRef"></p>,
-            <button onClick={this.showPostByTitle}>Show post by title</button>,
-            <p id="showByTitle"></p>,
+            <button onClick={this.getFriendByRef}>Get friend by ref</button>,
+            <p id="getByRef"></p>,
+            <button onClick={this.getFriendByName}>Get friend by name</button>,
+            <p id="getByName"></p>,
             <br />,
-            <button onClick={this.deleteAllPosts}>Delete all posts</button>,
-            <p id="deleteAllPosts"></p>,
+            <button onClick={this.deleteOneFriend}>Delete one friend</button>,
+            <p id="deleteOneFriend"></p>,
             <br />,
-            <button onClick={this.deleteOnePost}>Delete one post by title</button>,
-            <p id="deleteOnePost"></p>,
-            <br />,
-            <button onClick={this.showAllPosts}>Show all posts in console </button>,
-            <div id="showAllPosts"></div>
-
+            <button onClick={this.getAllFriends}>Get all friends </button>,
+            <div id="allFriends"></div>,
+            <button onClick={this.compareTwoFriends}>Are the all answers identical?(compare friends)</button>,
+            <p id="compare"></p>,
+            <br />
         ];
     }
 }
