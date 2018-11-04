8d8a4e344a461309495a0b6fc5ba00879a4844f7
Ion <19718355+Nei-V@users.noreply.github.com>
2018-11-04 03:43:55 +0200

    small progress on questions and answers, many bugs still

--- src/components/changingQuestions/Answer.jsx
+++ src/components/changingQuestions/Answer.jsx
@@ -1,29 +1,50 @@
 import React, { Component } from 'react';
 
-class Answer extends React.Component{
-    constructor(props){
+class Answer extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={answerToShow:""}
-        /* this.setState={
-            answerToShow:""
-        } */
-    }
-    
-    componentDidMount(){
-        let answerFromProp=this.props.prop;
-        console.log("answerFromProp",this.props.prop);
-        if (answerFromProp==100) {
+        this.state = { answerToShow: "" }
+        }
+
+    componentDidMount() {
+        let answerFromProp = this.props.answerArray;
+        let answerNo = this.props.answerNo;
+        console.log("answerFromProp", this.props.answerArray, "answerNumber(counter)", answerNo);
+        if (answerFromProp == 100) {
             this.setState((state, props) => ({
-                answerToShow:`<h1>THIS IS IT ${answerFromProp}</h1>`,
+                answerToShow: this.createInputFieldNumbers()
+
+            }));
+        } else {
+                this.setState((state, props) => ({
+                answerToShow: this.addSelectBox(),
             }));
         }
     }
+    createInputFieldNumbers() {
+        return (<input id="number" type="number" min="0" max="100"></input>)
+    }
+
+    addSelectBox() {
+        var sel = document.getElementById('answerList');
+        var opt = null;
+
+        for (let i = 0; i < this.props.answerArray.length; i++) {
+
+            opt = document.createElement('option');
+            opt.value = this.props.answerArray[i];
+            opt.innerHTML = this.props.answerArray[i];
+            sel.appendChild(opt);
+        }
+      }
 
     render() {
-        console.log("answer in answer is",this.state.answerToShow);
-        return (
-            <div>The answer is: {this.state.answerToShow}</div>
-              )
+        console.log("answer in answer is", this.state.answerToShow);
+        return [
+            <div id="answerOptions">{this.state.answerToShow}</div>,
+            <select id="answerList" />
+
+    ]
     }
 }
 
