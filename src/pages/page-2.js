8af0b63faa529dcd045e35d73ccc0690c1a48936
Akhil Alluri <alluriaa@amazon.com>
2018-11-05 14:31:59 +0530

    Layout fix for instructions

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,18 +3,32 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import WordGame from '../components/faunalink/WordGame.jsx';
+import Divider from '@material-ui/core/Divider';
+
+const divStyle = {
+  paddingTop: '20px'
+};
+
+const centerStyle = {
+  textAlign: 'center',
+  marginLeft: 'auto',
+  marginRight: 'auto'
+}
 
 const SecondPage = () => (
   <Layout>
-    <p>WORD GAME</p>
-    <WordGame/>
     <div>
-        <ul>
-        <h3>Objective</h3>          
-          <li>Each player takes turn creating words with the last letter of the submitted word.</li>
-          <li>A player wins when the other other player runs out of time.</li>
-          <li>Words cannot be repeated.</li>
-        </ul>
+      <p>Word game</p>
+      <WordGame/>
+    </div>
+    <div style={divStyle}>
+      <Divider/>
+      <h3>Objective</h3>  
+      <ul>        
+        <li>Each player takes turn creating words with the last letter of the submitted word.</li>
+        <li>A player wins when the other other player runs out of time.</li>
+        <li>Words cannot be repeated.</li>
+      </ul>
     </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
